b47033ab5c5dc0ccc7476f9f9883a103dcdccd50
najim2 <najim8469@gmail.com>
2020-03-04 21:29:05 +0600

    first commit

---
+++ null-undefined.js
@@ -0,0 +1,19 @@
+ let pakhi;
+ console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1+num2);
+
+}
+
+const result = add(12,85);
+console.log(result);
+
+const premik = {name:"najim", phone:0173375};
+console.log(premik.gf);
+
+const ages = [24, 25, 37];
+console.log(ages[5]);
+
+let fun = undefined;
+console.log(fun);
---
+++ true-false.js
@@ -0,0 +1,18 @@
+//const name = 1;
+//const name = 0;
+//let name = null;
+//console.log(name);
+// 0 
+// undefined
+// null
+// ""
+// NaN
+// Truthy
+let name = NaN;
+console.log(name);
+
+if(name){
+    console.log("Conditon is true");
+}else{
+    console.log("Conditon is false");
+}
