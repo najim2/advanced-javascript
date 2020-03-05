b8b50a44c1a57b80c2435ac75b1ca0bb71a5c7f0
najim2 <najim8469@gmail.com>
2020-03-05 11:43:07 +0600

    last commit

---
+++ scope.js
@@ -0,0 +1,61 @@
+// function sum(first, second){
+//     let result = first + second;
+//     return result;
+// }
+// const output = sum(23, 54);
+// console.log(output);
+// //console.log(result);
+
+// function sum(first, second){
+//     let result = first + second;
+//     console.log(result);
+//     return result;
+// }
+// const output = sum(23, 54);
+// console.log(output);
+
+// let bonus = 20;
+// function sum(first, second){
+//     let result = first + second + bonus;
+//     //console.log(result);
+//     if(result>9){
+//         let mood = "happy";
+//             mood = "franky";
+//             mood = "cranky"
+//             mood = "Banky"
+//         console.log(mood);
+//     }
+//     console.log(mood);
+//     return result;
+// }
+// const output = sum(5, 5);
+// // console.log(output);
+// // console.log(bonus);
+
+
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    //console.log(result);
+    if(result>9){
+        let mood = "happy";
+            mood = "franky";
+            mood = "cranky"
+            mood = "Banky"
+       // console.log(mood);
+    }
+    // console.log(day);
+    // var day ="friday";
+
+    // console.log(day);
+    // let day ="friday";
+
+    
+    let day ="friday";
+    console.log(day);
+    return result;
+}
+const output = sum(5, 5);
+// console.log(output);
+// console.log(bonus);
+
